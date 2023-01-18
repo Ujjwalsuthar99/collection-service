4f365c5376edd0c9749e78983658c49d45932fd1
Ajay Bhakar <ajay.bhakar@synoriq.in>
2023-01-18 15:57:25 +0530

    basic Spring Boot project setup

---
+++ src/test/java/com/synoriq/synofin/collection/collectionservice/CollectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synoriq.synofin.collection.collectionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
