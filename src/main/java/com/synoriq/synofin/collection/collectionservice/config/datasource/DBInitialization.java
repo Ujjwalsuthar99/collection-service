a6c8e4aa4fea19345d4ccc16a07e917dea002cef
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2023-02-14 12:15:58 +0530

    Debugging client id

--- src/main/java/com/synoriq/synofin/collection/collectionservice/config/datasource/DBInitialization.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/config/datasource/DBInitialization.java
@@ -64,15 +64,13 @@ public class DBInitialization {
                 connectionProperty.setProperty("maxActive", "100");
                 connectionProperty.setProperty("maxIdle", "10");
                 log.info("Datbase driver = " + getDbDriver());
-                if(client == "finova") {
-                    dataSource.setDriverClassName(getDbDriver());
-                    dataSource.setUrl(getDbUrl() + client + (getDbConnectionProperties() != null ? getDbConnectionProperties() : ""));
-                    dataSource.setUsername(getDbUserName());
-                    dataSource.setPassword(getDbPassword());
-                    dataSource.setConnectionProperties(connectionProperty);
-                    log.info("client datasource url = " + dataSource.getUrl());
-                    hashMap.put(client, dataSource);
-                }
+                dataSource.setDriverClassName(getDbDriver());
+                dataSource.setUrl(getDbUrl() + client + (getDbConnectionProperties() != null ? getDbConnectionProperties() : ""));
+                dataSource.setUsername(getDbUserName());
+                dataSource.setPassword(getDbPassword());
+                dataSource.setConnectionProperties(connectionProperty);
+                log.info("client datasource url = " + dataSource.getUrl());
+                hashMap.put(client, dataSource);
             }
         } else {
             throw new NullPointerException("Client Array not defined");
