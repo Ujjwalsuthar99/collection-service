43da76fa8fd6df952671d1a1844f2cf324777b7c
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2023-02-18 19:02:27 +0530

    collection configuration api has been created in collection service

---
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/controller/CollectionConfigurationController.java
@@ -0,0 +1,48 @@
+package com.synoriq.synofin.collection.collectionservice.controller;
+
+import com.synoriq.synofin.collection.collectionservice.common.errorcode.ErrorCode;
+import com.synoriq.synofin.collection.collectionservice.rest.request.CollectionConfigurationDtoRequest;
+import com.synoriq.synofin.collection.collectionservice.rest.response.BaseDTOResponse;
+import com.synoriq.synofin.collection.collectionservice.service.CollectionConfigurationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1")
+@EnableTransactionManagement
+@Slf4j
+public class CollectionConfigurationController {
+
+    @Autowired
+    CollectionConfigurationService collectionConfigurationService;
+
+    @RequestMapping(value = "/configurations", method = RequestMethod.GET)
+    public ResponseEntity<Object> getDashboardCountByUserId() throws SQLException {
+        BaseDTOResponse<Object> baseResponse;
+        ResponseEntity<Object> response = null;
+        List<CollectionConfigurationDtoRequest> result;
+
+        try {
+            result = collectionConfigurationService.getCollectionConfiguration();
+            baseResponse = new BaseDTOResponse<>(result);
+            response = new ResponseEntity<>(baseResponse, HttpStatus.OK);
+        } catch (Exception e) {
+            if (com.synoriq.synofin.collection.collectionservice.common.errorcode.ErrorCode.getErrorCode(Integer.valueOf(e.getMessage())) != null) {
+                baseResponse = new BaseDTOResponse<>(com.synoriq.synofin.collection.collectionservice.common.errorcode.ErrorCode.getErrorCode(Integer.valueOf(e.getMessage())));
+            } else {
+                baseResponse = new BaseDTOResponse<>(ErrorCode.DATA_FETCH_ERROR);
+            }
+            response = new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
+        }
+        return response;
+    }
+}
