70cf5806fbaf0bf91c47e70fdbaa570025650b2c
Jatin Chanchalani <jatin.chanchalani@synoriq.in>
2023-02-24 11:19:41 +0530

    add get activity name master

---
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/common/ActivityEvent.java
@@ -0,0 +1,21 @@
+package com.synoriq.synofin.collection.collectionservice.common;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ActivityEvent {
+
+    public static final String CREATE_RECEIPT = "create_receipt";
+    public  static final String CREATE_FOLLOWUP = "Create_Followup";
+    public static final String LOGIN = "Login";
+    public static final String ADD_CONTACT = "Add_Contact";
+    public static final String RECEIPT_TRANSFER = "Receipt_Transfer";
+    public static final String LOGOUT = "Logout";
+    public static final String CHANGE_PASSWORD = "Change_Password";
+    public static final String RESCHEDULE ="Reschedule";
+    public static final String CLOSED = "Closed";
+
+
+}
