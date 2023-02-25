370cb574c136acec26165bd9a270505d5c25d6bc
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2023-02-25 16:37:13 +0530

    add one more column in receiptTransferHistoryRepository

--- src/main/java/com/synoriq/synofin/collection/collectionservice/repository/ReceiptTransferRepository.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/repository/ReceiptTransferRepository.java
@@ -22,7 +22,7 @@ public interface ReceiptTransferRepository extends JpaRepository<ReceiptTransfer
     List<ReceiptTransferEntity> getReceiptTransferByUserId(@Param("transferredBy") Long transferredBy, @Param("fromDate") Date fromDate
             , @Param("toDate") Date toDate, @Param("status") String status);
 
-    @Query(nativeQuery = true,value = "select rt.transfer_mode , rt.transfer_bank_code , rt.transfer_type , rt.transferred_to_user_id , rt.status , rt.created_date , rt.amount , u.name as transferred_to_name " +
+    @Query(nativeQuery = true,value = "select rt.receipt_transfer_id, rt.transfer_mode, rt.transfer_bank_code , rt.transfer_type , rt.transferred_to_user_id , rt.status , rt.created_date , rt.amount , u.name as transferred_to_name " +
             "            from collection.receipt_transfer rt join master.users u on u.user_id = rt.transferred_to_user_id \n" +
             "            where (rt.transferred_by = :transferredBy or rt.transferred_to_user_id = :transferredBy) and rt.created_date between :fromDate and :toDate\n" +
             "            order by\n" +
@@ -33,7 +33,7 @@ public interface ReceiptTransferRepository extends JpaRepository<ReceiptTransfer
             , @Param("toDate") Date toDate);
 
 
-    @Query(nativeQuery = true,value = "select concat_ws(' ', c.first_name, c.last_name) as name, sr.form->>'receipt_amount' as receipt_amount, sr.service_request_id as receipt_id\n" +
+    @Query(nativeQuery = true,value = "select concat_ws(' ', c.first_name, c.last_name) as name, sr.form->>'receipt_amount' as receipt_amount, sr.service_request_id as receipt_id, sr.form->>'payment_mode' as payment_mode\n" +
             "         from collection.receipt_transfer rt \n" +
             "         join (select collection_receipts_id, receipt_transfer_id from collection.receipt_transfer_history) as rth on rt.receipt_transfer_id  = rth.receipt_transfer_id \n" +
             "         join (select service_request_id, loan_id, form from lms.service_request) as sr on sr.service_request_id = rth.collection_receipts_id \n" +
