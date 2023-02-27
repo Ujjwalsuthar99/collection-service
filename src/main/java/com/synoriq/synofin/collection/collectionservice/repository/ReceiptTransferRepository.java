8f897b65175de4491f22c5e2b094a6b1c577a155
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2023-02-27 18:01:54 +0530

    changes

--- src/main/java/com/synoriq/synofin/collection/collectionservice/repository/ReceiptTransferRepository.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/repository/ReceiptTransferRepository.java
@@ -25,9 +25,9 @@ public interface ReceiptTransferRepository extends JpaRepository<ReceiptTransfer
     @Query(nativeQuery = true,value = "select rt.receipt_transfer_id, rt.transfer_mode, rt.transfer_bank_code , rt.transfer_type , rt.transferred_to_user_id , rt.transferred_by, rt.status , rt.created_date , rt.amount , u.name as transferred_to_name " +
             "            ,case when rt.transferred_by = :transferredBy then 'transfer' else 'receiver' end as user_type, \n" +
             "            (case \n" +
-            "            when sr.status = 'pending' then 'blue'\n" +
-            "            when sr.status = 'receipt_transfer_approve' then 'green'\n" +
-            "            when sr.status = 'receipt_transfer_reject' then 'red'\n" +
+            "            when rt.status = 'pending' then 'blue'\n" +
+            "            when rt.status = 'receipt_transfer_approve' then 'green'\n" +
+            "            when rt.status = 'receipt_transfer_reject' then 'red'\n" +
             "            else 'black'\n" +
             "            end) as status_color_key\n" +
             "            from collection.receipt_transfer rt left join master.users u on u.user_id = rt.transferred_to_user_id \n" +
