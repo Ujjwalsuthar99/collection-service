de220f1a2b4161df4112fc3f360e7a68d5253dc8
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2023-02-28 23:13:18 +0530

    All user data pagination is done in MasterDataService.java activity name change for receipt transfer in ReceiptTransferRepository.java

--- src/main/java/com/synoriq/synofin/collection/collectionservice/service/MasterDataService.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/service/MasterDataService.java
@@ -1,9 +1,8 @@
 package com.synoriq.synofin.collection.collectionservice.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.synoriq.synofin.collection.collectionservice.entity.CollectionConfigurationsEntity;
-import com.synoriq.synofin.collection.collectionservice.rest.request.CollectionConfigurationDtoRequest;
 import com.synoriq.synofin.collection.collectionservice.rest.request.masterDTOs.MasterDtoRequest;
+import com.synoriq.synofin.collection.collectionservice.rest.response.BaseDTOResponse;
 import com.synoriq.synofin.collection.collectionservice.rest.response.ContactDTOResponse;
 import com.synoriq.synofin.collection.collectionservice.rest.response.UserDTOResponse;
 import com.synoriq.synofin.collection.collectionservice.rest.response.MasterDTOResponse;
@@ -14,6 +13,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
@@ -48,6 +48,7 @@ public class MasterDataService {
     public Object getUserDetail(String token, Integer page, Integer size) throws Exception {
 
         UserDTOResponse res = new UserDTOResponse();
+        BaseDTOResponse<Object> baseDTOResponse = null;
         try {
             HttpHeaders httpHeaders = new HttpHeaders();
             httpHeaders.add("Authorization", token);
@@ -60,18 +61,21 @@ public class MasterDataService {
                     .typeResponseType(UserDTOResponse.class)
                     .build().call();
 
-            log.info("responseData {}", res);
             List<Object> userData = res.getData();
             log.info("userData.toArray().length {}", userData.toArray().length);
-//            for (int i = 0; i < userData.toArray().length; i++) {
-//                log.info("userData Object {}", userData);
-//            }
+            int pageRequest = (page * size) - 10 ;
+            List<Object> pageableArr = new LinkedList<>();
+            for (int i = pageRequest; i < (pageRequest+10); i++) {
+                pageableArr.add(userData.get(i));
+            }
+            log.info("pageableArr {}", pageableArr);
+            baseDTOResponse = new BaseDTOResponse<>(pageableArr);
 
         } catch (Exception ee) {
             log.error("{}", ee.getMessage());
         }
 
-        return res;
+        return baseDTOResponse;
     }
     public Object getContactSupport(String token,String keyword, String model) throws Exception {
 
