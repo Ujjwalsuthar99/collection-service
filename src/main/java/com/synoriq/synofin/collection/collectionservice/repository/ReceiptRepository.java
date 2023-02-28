f1f3efdeb635f1767ebba2f13faeda889f24a922
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2023-02-28 13:34:09 +0530

    color changes in receipts

--- src/main/java/com/synoriq/synofin/collection/collectionservice/repository/ReceiptRepository.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/repository/ReceiptRepository.java
@@ -48,10 +48,10 @@ public interface ReceiptRepository extends JpaRepository<FollowUpEntity, Long> {
             "                cast(sr.form->>'receipt_amount' as decimal) as receipt_amount,\n" +
             "                sr.status as status,\n" +
             "                (case \n" +
-            "                    when sr.status = 'approved' then 'green'\n" +
-            "                    when sr.status = 'rejected' then 'red'\n" +
-            "                    when sr.status = 'initiated' then 'yellow'\n" +
-            "                    else 'black'\n" +
+            "                    when sr.status = 'approved' then '#229A16'\n" +
+            "                    when sr.status = 'rejected' then '#EC1C24'\n" +
+            "                    when sr.status = 'initiated' then '#2F80ED'\n" +
+            "                    else '#323232'\n" +
             "                end) as status_color_key\n" +
             "                from lms.service_request sr \n" +
             "                join collection.collection_receipts cr on cr.receipt_id = sr.service_request_id\n" +
