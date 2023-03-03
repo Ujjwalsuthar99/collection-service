3d84c4fc75d0a5166e647311d275a4f797c3afcd
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2023-03-03 18:28:35 +0530

    add receipts data in the query in CollectionActivityLogsRepository.java

--- src/main/java/com/synoriq/synofin/collection/collectionservice/repository/CollectionActivityLogsRepository.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/repository/CollectionActivityLogsRepository.java
@@ -21,8 +21,9 @@ public interface CollectionActivityLogsRepository extends PagingAndSortingReposi
     List<CollectionActivityLogsEntity> getActivityLogsUserWIseByDuration(@Param("userId") Long userId, @Param("fromDate")Date fromDate
             , @Param("toDate") Date toDate, Pageable pageable);
 
-    @Query(nativeQuery = true,value = "select * from collection.collection_activity_logs where loan_id = :loanId " +
-            "and activity_date between :fromDate and :toDate ")
+    @Query(nativeQuery = true,value = "select cal.collection_activity_logs_id , cal.activity_date , cal.activity_by , cal.activity_name, cal.distance_from_user_branch , cal.address , cal.images , cal.remarks , cal.loan_id , cal.geo_location_data,cr.receipt_id  \n" +
+            "from collection.collection_activity_logs cal left join collection.collection_receipts cr on cr.collection_activity_logs_id = cal.collection_activity_logs_id  " +
+            "where loan_id = :loanId and activity_date between :fromDate and :toDate ")
     List<CollectionActivityLogsEntity> getActivityLogsLoanWiseByDuration(@Param("loanId") Long loanId, @Param("fromDate")Date fromDate
             , @Param("toDate") Date toDate, Pageable pageable);
 
