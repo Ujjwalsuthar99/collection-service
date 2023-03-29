169e25dce718463b15fcc52364c51cf38a942131
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2023-03-29 17:32:09 +0530

    added DTO's

---
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/rest/response/userDetailByTokenDTOs/UserDetailDataByTokenDTO.java
@@ -0,0 +1,21 @@
+package com.synoriq.synofin.collection.collectionservice.rest.response.userDetailByTokenDTOs;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class UserDetailDataByTokenDTO {
+
+    @JsonProperty("user_type")
+    private String userType;
+
+    @JsonProperty("user_data")
+    private userDataDTO userData;
+
+    @JsonProperty("client_id")
+    private String clientId;
+
+    @JsonProperty("username")
+    private String userName;
+
+}
