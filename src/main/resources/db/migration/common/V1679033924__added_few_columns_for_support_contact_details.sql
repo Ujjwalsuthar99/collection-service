d3cc34d4ede9e2d6d1068e567a6374158a8dff5b
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2023-04-11 11:21:22 +0530

    Added a constant for online collection limit. Added a check for online collection limit. Added a migration file for new configurations.

--- src/main/resources/db/migration/common/V1679033924__added_few_columns_for_support_contact_details.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE master.support_contact_details ADD modified_by int8 NULL;
-ALTER TABLE master.support_contact_details ADD stage varchar NULL;
-ALTER TABLE master.support_contact_details ADD active bool NULL;
-ALTER TABLE master.support_contact_details ADD modified_date timestamptz NULL;
