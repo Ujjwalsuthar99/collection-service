f11f1ddba4759280bc0693531629bd0d51caabd6
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2023-04-17 18:27:35 +0530

    Added update action for subtracting cash in hand when we are receiving any event from LMS.

--- src/main/java/com/synoriq/synofin/collection/collectionservice/service/KafkaListnerService.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/service/KafkaListnerService.java
@@ -1,6 +1,8 @@
 package com.synoriq.synofin.collection.collectionservice.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.synoriq.synofin.collection.collectionservice.entity.CollectionLimitUserWiseEntity;
+import com.synoriq.synofin.collection.collectionservice.repository.CollectionLimitUserWiseRepository;
 import com.synoriq.synofin.collection.collectionservice.rest.request.masterDTOs.MasterDtoRequest;
 import com.synoriq.synofin.collection.collectionservice.rest.request.uploadImageOnS3.UploadImageOnS3RequestDTO;
 import com.synoriq.synofin.collection.collectionservice.rest.response.*;
@@ -10,7 +12,9 @@ import com.synoriq.synofin.collection.collectionservice.rest.response.userDetail
 import com.synoriq.synofin.collection.collectionservice.service.utilityservice.HTTPRequestService;
 import com.synoriq.synofin.events.common.interfaces.SynofinEventServiceListener;
 import com.synoriq.synofin.events.template.MessageContainerTemplate;
+import com.synoriq.synofin.events.template.lms.CollectionRequestActionEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.kafka.support.Acknowledgment;
@@ -30,11 +34,30 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 public class KafkaListnerService {
+
+    @Autowired
+    private CollectionLimitUserWiseRepository collectionLimitUserWiseRepository;
     @SynofinEventServiceListener(topics = "${spring.kafka.events.topic}", groupId = "collection", containerFactory = "kafkaListenerContainerFactory")
     public void consumerTest(@Payload MessageContainerTemplate message, @Headers MessageHeaders headers) {
         try {
 //            changeAnnotationValue();
-            log.info("message datatatatat ->  {}", message);
+            log.info("message datatatatat ->  {}", message.getMessage());
+            CollectionRequestActionEvent messageObject = new ObjectMapper().convertValue(message.getMessage(), CollectionRequestActionEvent.class);
+            log.info("message object, {}", messageObject);
+            CollectionLimitUserWiseEntity collectionLimitUser = (CollectionLimitUserWiseEntity) collectionLimitUserWiseRepository.getCollectionLimitUserWiseByUserId(messageObject.getUserId(), messageObject.getPaymentMode());
+            CollectionLimitUserWiseEntity collectionLimitUserWiseEntity = new CollectionLimitUserWiseEntity();
+
+            if(collectionLimitUser != null) {
+                collectionLimitUserWiseEntity.setCollectionLimitDefinitionsId(collectionLimitUser.getCollectionLimitDefinitionsId());
+                collectionLimitUserWiseEntity.setCreatedDate(new Date());
+                collectionLimitUserWiseEntity.setDeleted(collectionLimitUser.getDeleted());
+                collectionLimitUserWiseEntity.setCollectionLimitStrategiesKey(collectionLimitUser.getCollectionLimitStrategiesKey());
+                collectionLimitUserWiseEntity.setUserId(collectionLimitUser.getUserId());
+                collectionLimitUserWiseEntity.setTotalLimitValue(collectionLimitUser.getTotalLimitValue());
+                collectionLimitUserWiseEntity.setUtilizedLimitValue(collectionLimitUser.getUtilizedLimitValue() - Double.parseDouble(String.valueOf(messageObject.getReceiptAmount())));
+                log.info("collection limit user wise entity {}", collectionLimitUserWiseEntity);
+                collectionLimitUserWiseRepository.save(collectionLimitUserWiseEntity);
+            }
             log.info(" ---------- Things acknowledged -------------");
         } catch (Exception ee) {
             ee.printStackTrace();
