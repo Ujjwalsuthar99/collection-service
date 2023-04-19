d6ed5450b404825e18564779c1e00b8cc23119ce
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2023-04-19 16:19:30 +0530

    Fixed query for configurations.

--- src/main/resources/db/migration/common/V1681897264__added_new_configuration_for_payment_modes.sql
+++ src/main/resources/db/migration/common/V1681897264__added_new_configuration_for_payment_modes.sql
@@ -1,19 +1,19 @@
 INSERT INTO collection.collection_configurations
 (created_date, created_by, deleted, configuration_name, configuration_value, configuration_description)
-VALUES('2023-01-27 13:01:20.000', 1, false, 'payment_modes_to_use_at_create_receipt', 'coll_cash/coll_cheque/coll_upi', "List all the payment modes here to enable and disable them at the time of receipt generation for all users.");
+VALUES('2023-01-27 13:01:20.000', 1, false, 'payment_modes_to_use_at_create_receipt', 'coll_cash/coll_cheque/coll_upi', 'List all the payment modes here to enable and disable them at the time of receipt generation for all users.');
 
 INSERT INTO collection.collection_configurations
 (created_date, created_by, deleted, configuration_name, configuration_value, configuration_description)
-VALUES('2023-01-27 13:01:20.000', 1, false, 'create_receipt_payment_proof_image_mandate', 'false', "Set true or false to mandate the image upload field while creating receipt.");
+VALUES('2023-01-27 13:01:20.000', 1, false, 'create_receipt_payment_proof_image_mandate', 'false', 'Set true or false to mandate the image upload field while creating receipt.');
 
 INSERT INTO collection.collection_configurations
 (created_date, created_by, deleted, configuration_name, configuration_value, configuration_description)
-VALUES('2023-01-27 13:01:20.000', 1, false, 'create_receipt_selfie_with_customer_image_mandate', 'false', "Set true or false to mandate the image upload field while creating receipt.");
+VALUES('2023-01-27 13:01:20.000', 1, false, 'create_receipt_selfie_with_customer_image_mandate', 'false', 'Set true or false to mandate the image upload field while creating receipt.');
 
 INSERT INTO collection.collection_configurations
 (created_date, created_by, deleted, configuration_name, configuration_value, configuration_description)
-VALUES('2023-01-27 13:01:20.000', 1, false, 'receipt_transfer_sender_image_mandate', 'false', "Set true or false to mandate the image upload field while creating receipt.");
+VALUES('2023-01-27 13:01:20.000', 1, false, 'receipt_transfer_sender_image_mandate', 'false', 'Set true or false to mandate the image upload field while creating receipt.');
 
 INSERT INTO collection.collection_configurations
 (created_date, created_by, deleted, configuration_name, configuration_value, configuration_description)
-VALUES('2023-01-27 13:01:20.000', 1, false, 'receipt_transfer_receiver_approval_image_mandate', 'false', "Set true or false to mandate the image upload field while creating receipt.");
+VALUES('2023-01-27 13:01:20.000', 1, false, 'receipt_transfer_receiver_approval_image_mandate', 'false', 'Set true or false to mandate the image upload field while creating receipt.');
