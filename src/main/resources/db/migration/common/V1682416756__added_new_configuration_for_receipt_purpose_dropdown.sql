7d64f2ec7515d21e73b7b6f39bf289d8d0b5cdfd
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2023-04-25 15:39:30 +0530

    Added configuration for receipt purpose drop down to set the specific list or all.

---
+++ src/main/resources/db/migration/common/V1682416756__added_new_configuration_for_receipt_purpose_dropdown.sql
@@ -0,0 +1,3 @@
+INSERT INTO collection.collection_configurations
+(created_date, created_by, deleted, configuration_name, configuration_value, configuration_description)
+VALUES('2023-01-27 13:01:20.000', 1, false, 'receipt_purpose_dropdown_list', 'charges/closure/installment', 'Set the value as all OR set specific charges keys separated by forwards slash for receipt purpose dropdown');
