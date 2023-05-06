d9f33f5e7a6ca6a0cd0bcf2ddef3c6a47ad07773
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2023-05-06 12:19:22 +0530

    Added two new configurations for thermal receipt and overdue breakup.

---
+++ src/main/resources/db/migration/common/V1683355289__added_new_configurations_to_show_overdue_breakup_and_thermal_receipt_at_create_receipt.sql
@@ -0,0 +1,8 @@
+INSERT INTO collection.collection_configurations
+(created_date, created_by, deleted, configuration_name, configuration_value, configuration_description, modified_date, modified_by, active, stage)
+VALUES('2023-01-27 13:01:20.000', 1, false, 'view_overdue_breakup_at_create_receipt', 'false', 'If auto allocation is yes then we can show the breakup of overdue amount of the respective loan', '2023-04-26 12:21:28.543', NULL, true, NULL);
+
+
+INSERT INTO collection.collection_configurations
+(created_date, created_by, deleted, configuration_name, configuration_value, configuration_description, modified_date, modified_by, active, stage)
+VALUES('2023-01-27 13:01:20.000', 1, false, 'thermal_printer', 'false', 'To enable thermal receipt printing after create receipt.', '2023-04-26 12:21:28.543', NULL, true, NULL);
