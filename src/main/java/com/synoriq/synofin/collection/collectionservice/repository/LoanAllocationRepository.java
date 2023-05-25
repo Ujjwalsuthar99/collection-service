46d3a04932b7529ec72133439abf9a0c3147e9f9
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2023-05-25 13:07:04 +0530

    Fixed username for loan allocation query.

--- src/main/java/com/synoriq/synofin/collection/collectionservice/repository/LoanAllocationRepository.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/repository/LoanAllocationRepository.java
@@ -18,11 +18,11 @@ public interface LoanAllocationRepository extends JpaRepository<LoanAllocationEn
 
     @Query(nativeQuery = true, value = "select\n" +
             "\tla.allocated_to_user_id as id,\n" +
-            "\tu.\"name\" as full_name,\n" +
-            "\tu.employee_code as employee_code\n" +
+            "\tu.\"name\" as \"name\",\n" +
+            "\tu.username as \"employeeCode\"\n" +
             "from\n" +
             "\tcollection.loan_allocation la\n" +
-            "join (select user_id, employee_code, name from master.users) as u on\n" +
+            "join (select user_id, username, name from master.users) as u on\n" +
             "\tu.user_id = la.allocated_to_user_id\n" +
             "where\n" +
             "\tla.loan_id = :loanId\n" +
