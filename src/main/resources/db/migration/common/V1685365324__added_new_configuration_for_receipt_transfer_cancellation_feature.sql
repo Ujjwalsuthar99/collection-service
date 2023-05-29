c2d46a29ed5f8a838a752697562638aec6f64d6b
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2023-05-29 18:57:15 +0530

    Fixed SQL V1685365324__added_new_configuration_for_receipt_transfer_cancellation_feature.sql

--- src/main/resources/db/migration/common/V1685365324__added_new_configuration_for_receipt_transfer_cancellation_feature.sql
+++ src/main/resources/db/migration/common/V1685365324__added_new_configuration_for_receipt_transfer_cancellation_feature.sql
@@ -1,3 +1,3 @@
 INSERT INTO collection.collection_configurations
 (created_date, created_by, deleted, configuration_name, configuration_value, configuration_description, modified_date, modified_by, active, stage)
-VALUES('2023-05-18 18:41:04.340', 1, false, 'receipt_transfer_cancel_feature', 'true', 'Set true to enable the cancel receipt transfer button or set false to hide it from the detail view of receipt transfer at sender end, '2023-05-18 18:41:04.340', NULL, true, NULL);
+VALUES('2023-05-18 18:41:04.340', 1, false, 'receipt_transfer_cancel_feature', 'true', 'Set true to enable the cancel receipt transfer button or set false to hide it from the detail view of receipt transfer at sender end', '2023-05-18 18:41:04.340', NULL, true, NULL);
