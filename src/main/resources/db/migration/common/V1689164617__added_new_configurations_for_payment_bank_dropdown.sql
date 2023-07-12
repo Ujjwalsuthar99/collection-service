e20621c1c8ed3c01aa3dee5b9e3f848b6931fcd0
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2023-07-12 18:08:39 +0530

    some bug changes

--- src/main/resources/db/migration/common/V1689164617__added_new_configurations_for_payment_bank_dropdown.sql
+++ src/main/resources/db/migration/common/V1689164617__added_new_configurations_for_payment_bank_dropdown.sql
@@ -1,3 +1,7 @@
 INSERT INTO collection.collection_configurations
 (created_date, created_by, deleted, configuration_name, configuration_value, configuration_description, modified_date, modified_by, active, stage)
-VALUES('2023-01-27 13:01:20.000', 1, false, 'payment_bank_dropdowns', 'true', 'If the value is true, all the accounts that are present in LMS will be visible in dropdown else then values will be of payment_bank keys with slash', '2023-04-26 12:21:28.543', NULL, true, NULL);
+VALUES('2023-01-27 13:01:20.000', 1, false, 'deposit_reminder', 'true', 'Deposit reminder enabled disabled if receipts are not deposited then alert will show', '2023-04-26 12:21:28.543', NULL, true, NULL);
+
+INSERT INTO collection.collection_configurations
+(created_date, created_by, deleted, configuration_name, configuration_value, configuration_description, modified_date, modified_by, active, stage)
+VALUES('2023-01-27 13:01:20.000', 1, false, 'deposit_reminder_hours', '48', 'Deposit reminder in these hours', '2023-04-26 12:21:28.543', NULL, true, NULL);
