8c9ae283b014d05bdaa60b34f905f0200e017ff4
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2023-07-15 14:16:27 +0530

    username and battery percentage added

--- src/main/java/com/synoriq/synofin/collection/collectionservice/repository/CollectionActivityLogsRepository.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/repository/CollectionActivityLogsRepository.java
@@ -23,6 +23,8 @@ public interface CollectionActivityLogsRepository extends PagingAndSortingReposi
 
     @Query(nativeQuery = true,value = "select\n" +
             "\tcal.collection_activity_logs_id as collection_activity_logs_id,\n" +
+            "\t(select u.username from master.users u where u.user_id = cal.activity_by) as user_name,\n" +
+            "\tcal.battery_percentage,\n" +
             "\tcal.activity_date as activity_date,\n" +
             "\tcal.activity_by as activity_by,\n" +
             "\tcal.activity_name as activity_name,\n" +
@@ -50,8 +52,9 @@ public interface CollectionActivityLogsRepository extends PagingAndSortingReposi
     List<Map<String, Object>> getActivityLogsLoanWiseByDurationByFilter(@Param("loanId") Long loanId, @Param("fromDate") Date fromDate
             , @Param("toDate") Date toDate, @Param("filter") String filterBy, Pageable pageable);
 
-    @Query(nativeQuery = true,value = "select cal.collection_activity_logs_id as collection_activity_logs_id, cal.activity_date as activity_date, cal.activity_by as activity_by, cal.activity_name as activity_name, cal.distance_from_user_branch as distance_from_user_branch,\n" +
-            "CAST(cal.address as TEXT) as address, CAST(cal.images as TEXT) as images, cal.remarks as remarks, cal.loan_id as loan_id, CAST(cal.geo_location_data as TEXT) as geo_location_data, (case when cal.activity_name = 'create_receipt' then true else false end) as is_receipt, cr.receipt_id as receipt_id, COUNT(cal.collection_activity_logs_id) OVER () AS total_rows\n" +
+    @Query(nativeQuery = true,value = "select cal.collection_activity_logs_id as collection_activity_logs_id, (select u.username from master.users u where u.user_id = cal.activity_by) as user_name,\n" +
+            " cal.battery_percentage, cal.activity_date as activity_date, cal.activity_by as activity_by, cal.activity_name as activity_name, cal.distance_from_user_branch as distance_from_user_branch,\n" +
+            " CAST(cal.address as TEXT) as address, CAST(cal.images as TEXT) as images, cal.remarks as remarks, cal.loan_id as loan_id, CAST(cal.geo_location_data as TEXT) as geo_location_data, (case when cal.activity_name = 'create_receipt' then true else false end) as is_receipt, cr.receipt_id as receipt_id, COUNT(cal.collection_activity_logs_id) OVER () AS total_rows\n" +
             " from collection.collection_activity_logs cal left join collection.collection_receipts cr on cr.collection_activity_logs_id = cal.collection_activity_logs_id \n" +
             " where cal.loan_id = :loanId and cal.activity_date between :fromDate and :toDate order by cal.activity_date desc")
     List<Map<String, Object>> getActivityLogsLoanWiseByDuration(@Param("loanId") Long loanId, @Param("fromDate")Date fromDate
