19e8fc8e803200f023f91b5901f7103d95618c12
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2023-09-22 11:15:14 +0530

    fixed

--- src/main/resources/db/migration/common/V1695360951__added_new_configurations_for_side_navigation_panel_again.sql
+++ src/main/resources/db/migration/common/V1695360951__added_new_configurations_for_side_navigation_panel_again.sql
@@ -1,6 +1,2 @@
 
 DELETE FROM collection.collection_configurations WHERE configuration_value = 'side_navigation_panel';
-
-INSERT INTO collection.collection_configurations
-(created_date, created_by, deleted, configuration_name, configuration_value, configuration_description, modified_date, modified_by, active, stage)
-VALUES('2023-01-27 13:01:20.000', 1, false, 'side_navigation_panel', '["receipt_transfer"]', 'If the configuration is blank then the common buttons will visible else in the array ', '2023-04-26 12:21:28.543', NULL, true, NULL);
