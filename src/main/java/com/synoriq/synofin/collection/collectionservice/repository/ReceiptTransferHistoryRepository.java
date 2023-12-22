29dace5cbe127ffeccfdc70074090d5ee51f8e65
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2023-12-22 16:23:11 +0530

    Fixed all-receipt-transfer API.

--- src/main/java/com/synoriq/synofin/collection/collectionservice/repository/ReceiptTransferHistoryRepository.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/repository/ReceiptTransferHistoryRepository.java
@@ -36,7 +36,7 @@ public interface ReceiptTransferHistoryRepository extends JpaRepository<ReceiptT
 
 
     @Query(nativeQuery = true, value = "select\n" +
-            "\trt.receipt_transfer_id, rt.created_date, concat(uu.name, ' ', uu.username) as transfer_by_name, (case when rt.transferred_to_user_id is null then (select ba.bank_name from master.bank_accounts ba where ba.bank_account_id = cast(rt.transfer_bank_code as bigint)) else concat(u.name, ' ', u.username) end) as transfer_to_name,\n" +
+            "\trt.receipt_transfer_id, rt.created_date, concat(uu.name, ' ', uu.username) as transfer_by_name, (case when rt.transferred_to_user_id is null then (select ba.bank_name from master.bank_accounts ba where cast(ba.bank_account_id as text) = cast(rt.transfer_bank_code as text)) else concat(u.name, ' ', u.username) end) as transfer_to_name,\n" +
             "\trt.transfer_type, rt.amount as deposit_amount, ba2.bank_name , ba2.account_number, CAST(cal.geo_location_data as TEXT) as transfer_location_data, CAST(cal2.geo_location_data as text) as approval_location_data, rt.action_datetime,\n" +
             "\tCAST(rt.receipt_image as TEXT) as receipt_image, rt.status as status, (case when rt.status != 'pending' then (select username from master.users where user_id = rt.\"action_by\") else '' end) as approved_by, COUNT(rt.receipt_transfer_id) OVER () AS total_rows\n" +
             "from\n" +
@@ -46,11 +46,11 @@ public interface ReceiptTransferHistoryRepository extends JpaRepository<ReceiptT
             "left join (select bank_account_id, bank_name, account_number from master.bank_accounts) as ba2 on cast(ba2.bank_account_id as text) = rt.transfer_bank_code\n" +
             "join collection.collection_activity_logs cal on cal.collection_activity_logs_id = rt.collection_activity_logs_id \n" +
             "left join (select collection_activity_logs_id, geo_location_data from collection.collection_activity_logs) as cal2 on cal2.collection_activity_logs_id = rt.action_activity_logs_id\n" +
-            "where rt.transfer_type='bank' and rt.status in (:statusList)" +
+            "where rt.transfer_type='bank' and rt.status in (:statusList)\n" +
             "and (\n" +
             "\tLOWER(cast(rt.receipt_transfer_id as text)) like LOWER(concat('%', :searchKey, '%'))\n" +
             "\tor LOWER(uu.username) like LOWER(concat('%', :searchKey, '%'))\n" +
-            ")" +
+            ")\n" +
             "order by rt.created_date desc")
     List<Map<String, Object>> getAllBankTransfers(@Param("statusList") List<String> statusList, @Param("searchKey") String searchKey, Pageable pageable);
 
