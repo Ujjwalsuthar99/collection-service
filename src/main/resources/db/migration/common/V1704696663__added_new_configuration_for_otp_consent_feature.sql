9b87a02119a84fefc47391f7bd8a1a3240510fe3
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2024-01-08 12:44:33 +0530

    Added two new configurations for OTP consent.

---
+++ src/main/resources/db/migration/common/V1704696663__added_new_configuration_for_otp_consent_feature.sql
@@ -0,0 +1,3 @@
+INSERT INTO collection.collection_configurations (created_date,created_by,deleted,configuration_name,configuration_value,configuration_description,modified_date,modified_by,active,stage) VALUES
+	 ('2024-01-08 17:04:11.345',1,false,'otp_consent','false','This configuration will enable and disable the otp consent feature before collect money, specify false to disable, applicant or co-applicant or guarantor or other to send to from , any to send the consent.',NULL,NULL,true,NULL),
+	 ('2024-01-08 17:05:07.449',1,false,'otp_consent_after_x_hours','19','Set time in 24 hours format to send the otp consent message to customer after the time you have selected',NULL,NULL,true,NULL);
