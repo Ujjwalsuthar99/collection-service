48729fea7694eefd269bb85949f747b42bea8724
ujjwalsuthar99 <ujjwal.suthar@synoriq.in>
2024-01-10 15:19:02 +0530

    remove requiredargsconstructor

--- src/main/java/com/synoriq/synofin/collection/collectionservice/rest/request/repossessionDTOs/lmsRepossession/LmsRepossessionDTO.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/rest/request/repossessionDTOs/lmsRepossession/LmsRepossessionDTO.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class LmsRepossessionDTO {
 
     @JsonProperty("service_request_type")
