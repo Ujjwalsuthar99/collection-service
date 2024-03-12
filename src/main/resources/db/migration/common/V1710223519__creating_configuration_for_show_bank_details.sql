7427bafc3c445c654647b3e6852dd3225b56feea
Sompalle.sekhar@synoriq.in <GunA@049>
2024-03-12 11:57:31 +0530

    added configuration show the bank details neft/rtgs

---
+++ src/main/resources/db/migration/common/V1710223519__creating_configuration_for_show_bank_details.sql
@@ -0,0 +1,2 @@
+INSERT INTO collection.collection_configurations (created_date,created_by,deleted,configuration_name,configuration_value,configuration_description,modified_date,modified_by,active,stage) VALUES
+	 ('2024-03-12 17:04:11.345',1,false,'show_bank_details_in_neft_rtgs','true','This configuration will show and hide the view bank details in neft_rtgs in digital payment',NULL,NULL,true,NULL);
