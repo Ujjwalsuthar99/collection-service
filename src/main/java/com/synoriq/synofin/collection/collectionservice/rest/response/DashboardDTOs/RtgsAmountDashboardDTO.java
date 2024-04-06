924b509d91f7432d0daae149e4536a3726f82e3f
Ujjwal Suthar <ujjwal.suthar@synoriq.in>
2024-04-06 13:34:05 +0530

    added DashboardDTOs/NeftAmountDashboardDTO.java

---
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/rest/response/DashboardDTOs/RtgsAmountDashboardDTO.java
@@ -0,0 +1,21 @@
+package com.synoriq.synofin.collection.collectionservice.rest.response.DashboardDTOs;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@AllArgsConstructor
+public class RtgsAmountDashboardDTO {
+
+    @JsonProperty("rtgs_amount")
+    private Double rtgsAmount;
+
+    @JsonProperty("rtgs_limit")
+    private Double rtgsLimit;
+
+}
