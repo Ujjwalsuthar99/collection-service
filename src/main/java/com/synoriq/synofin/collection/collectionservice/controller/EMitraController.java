34e6361b3531376fe6b82202bb40b480b1ab47bf
Shivam Kulshrestha <shivam.kulshrestha@synoriq.in>
2024-06-17 19:43:44 +0530

    Added request mapping for v1.

--- src/main/java/com/synoriq/synofin/collection/collectionservice/controller/EMitraController.java
+++ src/main/java/com/synoriq/synofin/collection/collectionservice/controller/EMitraController.java
@@ -2,8 +2,10 @@ package com.synoriq.synofin.collection.collectionservice.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/v1")
 public class EMitraController {
 
     @GetMapping("/e-mitra")
